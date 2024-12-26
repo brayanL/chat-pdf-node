cb48dcd0f3db62737026a9cdba2b3d8c22ef94c7
brayanL <godsarmybl@gmail.com>
2024-12-26 17:24:43 -0500

    fix: update how the library is exported.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export { ChatWithPdf } from './chat-with-pdf';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const ChatWithPdf = require('./chat-with-pdf');
+module.exports = { ChatWithPdf };
